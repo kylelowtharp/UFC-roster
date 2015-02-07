ad389e1c5a4bcd1156095db18a1fac7a090e99d3
Brian Capouch <brianc@saintjoe.edu>
2015-02-07 17:17:05 -0500

    Mapping example

---
+++ oldest.js
@@ -0,0 +1,16 @@
+var ANCESTRY_FILE = require('./ancestry.js');
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+function map(array, transform) {
+  var mapped = [];
+  for (var i = 0; i < array.length; i++)
+    mapped.push(transform(array[i]));
+  return mapped;
+}
+
+var overNinety = ancestry.filter(function(person) {
+  return person.died - person.born > 90;
+});
+console.log(map(overNinety, function(person) {
+  return person.name;
+}));
