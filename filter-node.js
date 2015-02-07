8e44446a711a96507833ead3a22e69c0101d6c75
Brian Capouch <brianc@saintjoe.edu>
2015-02-07 17:16:39 -0500

    Two versions of the filter example

---
+++ filter-node.js
@@ -0,0 +1,15 @@
+var ANCESTRY_FILE = require('./ancestry.js');
+var ancestry = JSON.parse(ANCESTRY_FILE);
+function filter(array, test) {
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i]))
+      passed.push(array[i]);
+  }
+  return passed;
+}
+
+console.log(filter(ancestry, function(person) {
+  return person.born > 1900 && person.born < 1925 ;
+}));
+
