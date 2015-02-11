ff4455d340a591ec30c485183d55f3c8d3d215e7
kylelowtharp <klv9700@saintjoe.edu>
2015-02-11 12:03:41 -0500

    UFC roster for data sets program

---
+++ UFCroster.js
@@ -0,0 +1,21 @@
+var UFCROSTER_FILE = "[\n " + [
+  '{"name": "Jon Jones", "class": "Light Heavyweight", "wins": 21, "losses": 1, "age": 27}',
+ '{"name": "ALexander Gustafsson", "class": "Light Heavyweight", "wins": 16, "losses": 3, "age": 28}',
+ '{"name": "Anthony Johnson", "class": "Light Heavyweight", "wins": 19, "losses": 4, "age": 30}',
+ '{"name": "Robbie Lawler", "class": "Welterweight", "wins": 25, "losses": 10, "age": 32}',
+ '{"name": "Rory Macdonald", "class": "Welterweight", "wins": 18, "losses": 2, "age": 25}',
+ '{"name": "Tyron Woodley", "class": "Welterweight", "wins": 15, "losses": 3, "age": 32}',
+ '{"name": "Jose Aldo", "class": "Featherweight", "wins": 25, "losses": 1, "age":28}',
+ '{"name": "Conor McGregor", "class": "Featherweight", "wins": 17, "losses": 2, "age": 26}',
+ '{"name": "Chad Mendes", "class": "Featherweight", "wins": 16, "losses": 2, "age": 29}',
+ '{"name": "Cain Velasquez", "class": "Heavyweight", "wins": 13, "losses": 1, "age": 32}',
+ '{"name": "Fabricio Werdum", "class": "Heavyweight", "wins": 19, "losses": 5, "age": 37}',
+ '{"name": "Travis Browne", "class": "Heavyweight", "wins": 17, "losses": 2, "age": 32}',
+ '{"name": "Chris Weidman", "class": "Middleweight", "wins": 12, "losses": 0, "age": 30}',
+ '{"name": "Anderson Silva", "class": "Middleweight", "wins": 34, "losses": 6, "age": 39}',
+ '{"name": "Vitor Belfort", "class": "Middleweight", "wins": 24, "losses": 10, "age": 37}', 
+].join(",\n ") + "\n]";
+
+//list of UFC fighters and statistics
+//moves var roster into filter.js
+
